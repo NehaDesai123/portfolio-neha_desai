83ac3009d39ae65962ed6849b40810007542c0ac
Neha <37369276+NehaDesai123@users.noreply.github.com>
2025-12-12 12:07:17 +0530

    Set base path for Vite configuration to '/portfolio-neha_desai/'

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/portfolio-neha_desai/',
   css: {
     postcss: './postcss.config.js'
   },
